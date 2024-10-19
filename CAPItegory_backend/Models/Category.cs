9078df9f19708e03244d4acafeacd8ed19c67479
bingieti <etienne.binginot@univ-rouen.fr>
2024-10-19 08:54:34 +0200

    feat: create category entity

---
+++ CAPItegory_backend/Models/Category.cs
@@ -0,0 +1,13 @@
+﻿namespace CAPItegory_backend.Models
+{
+    public class Category
+    {
+        public Guid Id { get; set; }
+
+        public string? Name { get; set; }
+
+        public Category? Parent { get; set; }
+
+        public DateTime? CreationDate { get; set; }
+    }
+}
